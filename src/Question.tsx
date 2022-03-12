4714e36239d330b1c752b7725fa3de7e18cebc3f
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-03-13 00:49:36 +0200

    use the children prop with Page and PageTitle components

--- src/Question.tsx
+++ src/Question.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { QuestionData } from './QuestionData';
 interface Props {
   data: QuestionData;
+  showContent?: boolean;
 }
-export const Question = ({ data }: Props) => (
+export const Question = ({ data, showContent = true }: Props) => (
   <div>
     <div>{data.title}</div>
+    {showContent && (
+      <div>
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
     <div>
       {`Asked by ${data.userName} on
     ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}
