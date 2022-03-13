fd75ad846ff86da249f65ca0e43b04370c1a5c45
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-03-13 23:56:27 +0200

    implement a function prop

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -9,6 +9,9 @@ export const HomePage = () => (
       <PageTitle>Unanswered Questions</PageTitle>
       <button>Ask a question</button>
     </div>
-    <QuestionList data={getUnansweredQuestions()} />
+    <QuestionList
+      data={getUnansweredQuestions()}
+      renderItem={(question) => <div>{question.title}</div>}
+    />
   </Page>
 );
