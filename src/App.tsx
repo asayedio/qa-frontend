352425b21aabce51503ddf91a46169e32de8d111
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-04-14 01:30:14 +0200

    lazy loading for ask page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AskPage } from './AskPage';
+import React from 'react';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 /**import './App.module.css';*/
@@ -10,6 +10,8 @@ import { HomePage } from './HomePage';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
 import { fontFamily, fontSize, gray2 } from './styles';
+import { LoadingSpinner } from './spinner';
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   return (
@@ -25,7 +27,14 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense fallback={<LoadingSpinner></LoadingSpinner>}>
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
