b758bc575ea6184a2907022a977fcc62fcfed21b
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-04-15 14:10:12 +0200

    convert the header component to be controlled

--- src/header.tsx
+++ src/header.tsx
@@ -1,13 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { fontFamily, fontSize, gray1, gray2, gray5 } from './styles';
 import React from 'react';
 import { UserIcon } from './Icons';
 /**import './Header.module.css';*/
 export const Header = () => {
+  const [searchParams] = useSearchParams();
+  const criteria = searchParams.get('criteria') || '';
+  const [search, setSearch] = React.useState(criteria);
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
+    setSearch(e.currentTarget.value);
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(search);
   };
   return (
     <div
@@ -36,26 +43,29 @@ export const Header = () => {
       >
         Q & A
       </Link>
-      <input
-        type="type"
-        placeholder="Searching..."
-        onChange={handleSearchInputChange}
-        css={css`
-          box-sizing: border-box;
-          font-family: ${fontFamily};
-          font-size: ${fontSize};
-          padding: 8px 10px;
-          border: 1px solid ${gray5};
-          border-radius: 3px;
-          color: ${gray2};
-          background-color: white;
-          width: 200px;
-          height: 30px;
-          :focus {
-            outline-color: ${gray5};
-          }
-        `}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="type"
+          placeholder="Searching..."
+          value={search}
+          onChange={handleSearchInputChange}
+          css={css`
+            box-sizing: border-box;
+            font-family: ${fontFamily};
+            font-size: ${fontSize};
+            padding: 8px 10px;
+            border: 1px solid ${gray5};
+            border-radius: 3px;
+            color: ${gray2};
+            background-color: white;
+            width: 200px;
+            height: 30px;
+            :focus {
+              outline-color: ${gray5};
+            }
+          `}
+        />
+      </form>
       <Link
         to="signin"
         css={css`
