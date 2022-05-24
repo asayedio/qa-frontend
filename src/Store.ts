67d37acf96572d5f2383ef6cc96e6ad7036aed28
asayed <asayed@amcham.org.eg>
2022-05-24 10:23:08 +0200

    create store's actions

--- src/Store.ts
+++ src/Store.ts
@@ -14,3 +14,62 @@ const initialQuestionState: QuestionsState = {
   viewing: null,
   searched: [],
 };
+
+// #region Actions
+
+// #region Getting Unanswered Questions
+
+// Indicates that unanswered questions are being fetched from the server
+export const GETTINGUNANSWEREDQUESTIONS = 'GettingUnansweredQuestions';
+
+export const gettingUnansweredQuestionsAction = () =>
+  ({
+    type: GETTINGUNANSWEREDQUESTIONS,
+  } as const);
+
+// Returns the action for when the unanswered questions have been retrieved from the server
+export const GOTUNANSWEREDQUESTIONS = 'GotUnansweredQuestions';
+export const gotUnansweredQuestionsAction = (questions: QuestionData[]) =>
+  ({
+    type: GOTUNANSWEREDQUESTIONS,
+    questions: questions,
+  } as const);
+// #endregion Getting Unanswered Questions
+
+// #region Viewing a question
+
+// Indicates that question is being fetched from the server
+export const GETTINGQUESTION = 'GettingQuestion';
+export const gettingQuestionAction = () =>
+  ({
+    type: GETTINGQUESTION,
+  } as const);
+
+// Returns the action for when the questions has been retrieved from the server
+export const GOTQUESTION = 'GotQuestion';
+export const gotQuestionAction = (question: QuestionData | null) =>
+  ({
+    type: GOTQUESTION,
+    question: question,
+  } as const);
+// #endregion Viewing a question
+
+// #region Searching questions
+
+// Indicates that questions is being searched
+export const SEARCHINGQUESTIONS = 'SearchingQuestions';
+export const searchingQuestionsAction = () =>
+  ({
+    type: SEARCHINGQUESTIONS,
+  } as const);
+
+// Returns the action for when the questions has been searched
+export const SEARCHEDQUESTIONS = 'SearchedQuestions';
+export const searchedQuestionsAction = (questions: QuestionData[]) =>
+  ({
+    type: SEARCHEDQUESTIONS,
+    questions,
+  } as const);
+// #endregion Searching questions
+
+// #endregion Actions
